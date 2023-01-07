d0649e3d6433c5ad277cdb451d7830a1a7b22da0
Li Yuan <e0985877@u.nus.edu>
2023-01-07 17:30:45 +0800

    my first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::API
     @current_user = User.find(session[:user_id]) if session[:user_id]
     @current_user
   end
+
+  def fallback_index_html
+    render :file => "public/index.html"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
       get "/logged_in", to: "sessions#is_logged_in?"
+
+      get "*path", to: "application#fallback_index_html", constraints: ->(request) do
+                     !request.xhr? && request.format.html?
+                   end
     end
   end
 end
