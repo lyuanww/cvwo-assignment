e0ca7d23cc5899558a494a3eaa2aaaabf98d88c6
Li Yuan <e0985877@u.nus.edu>
2023-01-07 22:45:10 +0800

    edit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "https://post-it-app.herokuapp.com/"
+    origins "http://localhost:3001", "https://post-it-app.herokuapp.com"
     resource "*",
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
