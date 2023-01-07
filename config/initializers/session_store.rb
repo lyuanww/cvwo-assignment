57fdf7ea6de146d5aef105f28b452102a02ef661
Li Yuan <e0985877@u.nus.edu>
2023-01-07 23:59:44 +0800

    edit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_myapp", tld_length: 2
+Rails.application.config.session_store :cookie_store, key: "_myapp", tld_length: 2, same_site: :none, secure: true
